be645e50216e6a9128371d5f3c329125d5379904
NikitaLev <nikita.leventev97@gmail.com>
2023-03-17 11:43:32 +0300

    STT tensorflow error omegaconf.errors.ConfigAttributeError: Missing key tf silero

--- stt_silero.py
+++ stt_silero.py
@@ -1,14 +1,15 @@
-import onnx
+import os
 import torch
-import onnxruntime
+import subprocess
+import tensorflow as tf
+import tensorflow_hub as tf_hub
 from omegaconf import OmegaConf
-
 
 
 def silero_stt_test():
     language = 'en'  # also available 'de', 'es'
 
-    # load provided utils
+    # load provided utils using torch.hub for brevity
     _, decoder, utils = torch.hub.load(repo_or_dir='snakers4/silero-models', model='silero_stt', language=language)
     (read_batch, split_into_batches,
      read_audio, prepare_model_input) = utils
@@ -20,11 +21,10 @@ def silero_stt_test():
     available_languages = list(models.stt_models.keys())
     assert language in available_languages
 
-    # load the actual ONNX model
-    torch.hub.download_url_to_file(models.stt_models.en.latest.onnx, 'model.onnx', progress=True)
-    onnx_model = onnx.load('model.onnx')
-    onnx.checker.check_model(onnx_model)
-    ort_session = onnxruntime.InferenceSession('model.onnx')
+    # load the actual tf model
+    torch.hub.download_url_to_file(models.stt_models.en.latest.tf, 'tf_model.tar.gz')
+    subprocess.run('rm -rf tf_model && mkdir tf_model && tar xzfv tf_model.tar.gz -C tf_model', shell=True, check=True)
+    tf_model = tf.saved_model.load('tf_model')
 
     # download a single file in any format compatible with TorchAudio
     torch.hub.download_url_to_file('https://opus-codec.org/static/examples/samples/speech_orig.wav',
@@ -33,9 +33,6 @@ def silero_stt_test():
     batches = split_into_batches(test_files, batch_size=10)
     input = prepare_model_input(read_batch(batches[0]))
 
-    # actual ONNX inference and decoding
-    onnx_input = input.detach().cpu().numpy()
-    ort_inputs = {'input': onnx_input}
-    ort_outs = ort_session.run(None, ort_inputs)
-    decoded = decoder(torch.Tensor(ort_outs[0])[0])
-    print(decoded)
+    # tf inference
+    res = tf_model.signatures["serving_default"](tf.constant(input.numpy()))['output_0']
+    print(decoder(torch.Tensor(res.numpy())[0]))
